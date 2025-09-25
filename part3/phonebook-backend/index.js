7bf5ccb79ab8aa8578b7e2ddd24b128e39430d31
Darustet <58421506+Darustet@users.noreply.github.com>
2025-09-25 13:18:23 +0300

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -76,7 +76,7 @@ app.get('/info', (request, response) => {
   })
 })
 
-app.post('/api/persons', (request, response) => {  
+app.post('/api/persons', (request, response, next) => {  
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -93,6 +93,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -120,7 +121,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
