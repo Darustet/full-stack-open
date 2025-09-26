83fc79d90848f186082b2654d636caaede77c711
Darustet <58421506+Darustet@users.noreply.github.com>
2025-09-26 12:57:17 +0300

    Part4 started, ex4.1 done
    Created bloglist project

---
+++ part4/blog-list/backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://fullstack:fullstackpassword@cluster0.9qllj1l.mongodb.net/bloglist?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+    console.log(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+  console.log(blog)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
