17f273e4288c04a9515d278898b8435173fa9734
Darustet <58421506+Darustet@users.noreply.github.com>
2025-10-06 11:02:13 +0300

    4.17 done

--- part4/blog-list/backend/controllers/blogs.js
+++ part4/blog-list/backend/controllers/blogs.js
@@ -1,23 +1,38 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 /* blogsRouter.get('/', (request, response) => {
   response.send('<h1>Welcome to Blog List!</h1>')
 }) */
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  //const user = await User.findById(body.id)
+
+  let user = await User.findById(body.user)
+
+  console.log('User from request body:', user)
+
+  if (!user) {
+    users = await User.aggregate([{ $sample: { size: 1 } }])
+    user = await User.findById(users[0]._id)
+    console.log('No user specified, selected random user:', user.username)
+  }
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: user._id
   })
 
   if (!blog.title || !blog.url) {
@@ -25,6 +40,9 @@ blogsRouter.post('/', async (request, response) => {
   }
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
